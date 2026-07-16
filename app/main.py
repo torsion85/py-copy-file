e95daed639b8accb1bd76dd7e31863b20c36fa72
Andrii Cherniienko <chern.combat32@gmail.com>
2026-07-16 16:39:49 +0300

    Done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import os
+
+
 def copy_file(command: str) -> None:
     parts = command.split()
-    if len(parts) < 3 or parts[0] != "cp":
+    if len(parts) != 3 or parts[0] != "cp":
         return
 
     source_file = parts[1]
@@ -9,9 +12,9 @@ def copy_file(command: str) -> None:
     if source_file == destination_file:
         return
 
-    try:
-        with open(source_file, "r") as file_in, \
-                open(destination_file, "w") as file_out:
-            file_out.write(file_in.read())
-    except FileNotFoundError:
+    if not os.path.exists(source_file):
         return
+
+    with open(source_file, "r") as file_in, \
+            open(destination_file, "w") as file_out:
+        file_out.write(file_in.read())
